e76e006784e080c32906683d08b9824beb3961de
MeliorDD <dorzhiev-danil@mail.ru>
2021-07-17 16:20:52 +0700

    without block of buttons

--- js/script.js
+++ js/script.js
@@ -1,17 +1,19 @@
 let getFile = document.getElementById('add-file');
 getFile.onchange = () => readFile(getFile);
-let jsonObject;
 
 function readFile (input){
     let file = input.files[0];
     if(checkExtension(file) != 'json'){
-        alert("Не верный формат файла!")
+        alert("Неверный формат файла!")
     }
     else{
         let reader = new FileReader();
         reader.readAsText(file);
         reader.onload = function() {
+            let jsonObject;
             jsonObject = JSON.parse(reader.result);
+            clearForm()
+            builder(jsonObject)
         };
         reader.onerror = function() {
             console.log(reader.error);
@@ -24,3 +26,73 @@ function checkExtension(file){
     return file.name.split(".").reverse()[0] 
 }
 
+function builder(jsonObj){
+    let form = document.querySelector(".form")
+    form.style.display = "block"
+    document.querySelector(".button-add-file").style.height = "fit-content"
+    
+    for (let key in jsonObj){
+        switch(key){
+            case "name":
+                let name = document.createElement('h1')
+                name.innerText = jsonObj[key]
+                form.appendChild(name)
+                break
+            case "fields":
+                let fields = document.createElement("div")
+                fields.className = "fields"
+                jsonObj[key].forEach(item => {
+                    if("label" in item){
+                        let labelToAdd = document.createElement("label")
+                        labelToAdd.innerText = item.label
+                        fields.appendChild(labelToAdd)
+                    }
+                    if("input" in item){
+                        let inputToAdd = document.createElement("input")
+                        for(let prop in item.input){
+                            inputToAdd.setAttribute(prop, item.input[prop])
+                        }
+                        fields.appendChild(inputToAdd)
+                    }
+                })
+                form.appendChild(fields)
+                break
+            case "references":
+                let references = document.createElement("div")
+                references.className = "references"
+                jsonObj[key].forEach(item => {
+                    for(let el in item){
+                        switch (el){
+                            case "input":
+                                let inputToAdd = document.createElement("input")
+                                for(let prop in item.input){
+                                    inputToAdd.setAttribute(prop, item.input[prop])
+                                }
+                                references.appendChild(inputToAdd)
+                                break
+                            case "text without ref":
+                                let spanToAdd = document.createElement("span")
+                                spanToAdd.innerText = item[el]
+                                references.appendChild(spanToAdd)
+                                break
+                            case "text":
+                                let refToAdd = document.createElement("a")
+                                refToAdd.innerText = item.text
+                                refToAdd.setAttribute("href", item.ref) 
+                                references.appendChild(refToAdd)
+                                break
+                        }
+                    }
+                })
+                form.appendChild(references)
+                break
+        }   
+    } 
+}
+
+function clearForm(){
+    let form = document.querySelector(".form")
+    while(form.firstChild){
+        form.removeChild(form.firstChild);
+    }
+}
